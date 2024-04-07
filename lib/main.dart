c36730df24fc38472da35949bfb6414b52dd4a85
alexmortelsison <alexmortelsison@gmail.com>
2024-04-07 11:50:49 +0900

    updated UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_true_or_false/quizzly.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const Quiz());
@@ -14,6 +14,38 @@ class Quiz extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Quizzly(),
+      ),
+    );
+  }
+}
+
+class Quizzly extends StatefulWidget {
+  const Quizzly({super.key});
+
+  @override
+  State<Quizzly> createState() => _QuizzlyState();
+}
+
+class _QuizzlyState extends State<Quizzly> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Container(
+        decoration: const BoxDecoration(color: Colors.black),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Question 1",
+                style: GoogleFonts.lato(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
